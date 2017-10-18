64a44cec3823f01c722acf41eec350f5936ea181
pranav45 <pranav.mainkar896@gmail.com>
2017-10-18 16:54:16 +0530

    add settings.local file[#151970511]

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -1,29 +1,27 @@
 <?php
 
 /**
- * Load services definition file.
- */
+* Load services definition file.
+*/
 $settings['container_yamls'][] = __DIR__ . '/services.yml';
 
 /**
- * Include the Pantheon-specific settings file.
- *
- * n.b. The settings.pantheon.php file makes some changes
- *      that affect all envrionments that this site
- *      exists in.  Always include this file, even in
- *      a local development environment, to insure that
- *      the site settings remain consistent.
- */
+* Include the Pantheon-specific settings file.
+*
+* n.b. The settings.pantheon.php file makes some changes
+*      that affect all envrionments that this site
+*      exists in.  Always include this file, even in
+*      a local development environment, to insure that
+*      the site settings remain consistent.
+*/
 include __DIR__ . "/settings.pantheon.php";
-$databases['default']['default'] = array (
-  'database' => 'champ',
-  'username' => 'root',
-  'password' => 'password',
-  'prefix' => '',
-  'host' => 'localhost',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
-$settings['hash_salt'] = 'd4xjpAeumfAM6ZOCkwO_VR1qdDZcqTiN7nB2E5l868vhdpjWF1waVk9arqwXWiUH4z32Dj5bZg';
+
+/**
+* If there is a local settings file, then include it
+*/
+$local_settings = __DIR__ . "/settings.local.php";
+if (file_exists($local_settings)) {
+ include $local_settings;
+}
+$settings['hash_salt'] = 'EChXW5_iqSHDC23nVXdvaEWyvjSuDB7L-qOmDKPBsW066jGi0u5G7gwQF0eXCny5TcX6x9i7aA';
 $settings['install_profile'] = 'champblog';
